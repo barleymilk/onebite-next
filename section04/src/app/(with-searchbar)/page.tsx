57d9fc0878a2289f62339efb22cfa19f8c0df54f
barleymilk <barleymilk640@gmail.com>
2025-10-14 13:41:52 +0900

    라우트 세그먼트 옵션 알아보기

--- section04/src/app/(with-searchbar)/page.tsx
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -2,6 +2,13 @@ import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import { BookData } from "@/types";
 
+// export const dynamic = "error";
+// 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
+// 1. auto : 기본값. 아무 것도 강제하지 않음.
+// 2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
+// 3. force-static : 페이지를 강제로 Static 페이지로 설정 -> 쿼리값에 의존하는 페이지에서 검색 동작이 제대로 되지 않는 부작용이 일어남.
+// 4. error : 페이지를 강제로 Static 페이지로 설정 (설정하면 안 되는 이유가 있다면 빌드 오류를 발생시킴)
+
 async function AllBooks() {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
