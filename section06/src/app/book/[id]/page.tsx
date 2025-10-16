cb828f1b6261dac212fb122f878744486026723f
barleymilk <barleymilk640@gmail.com>
2025-10-16 13:16:29 +0900

    book/[id] 페이지 컴포넌트 분리

--- section06/src/app/book/[id]/page.tsx
+++ section06/src/app/book/[id]/page.tsx
@@ -2,27 +2,13 @@ import { BookData } from "@/types";
 import style from "./page.module.css";
 import { notFound } from "next/navigation";
 
-// 1, 2, 3 값 외에는 url 파라미터가 존재하지 않아야 한다고 해석.
-// -> book/4부터는 DB에 있더라도 바로 404페이지로 바로 감
-// export const dynamicParams = false;
-
-// 정적인 파라미터 생성하는 함수
-// 빌드 타임에 만들게 됨 -> 동적 경로 갖는 페이지 -> 풀 라우트 캐시
-// [ 주의사항! ]
-// 1. 문자열로만 명시
-// 2. 무조건 해당하는 페이지가 static 페이지로 강제로 정해짐.
 export function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }];
 }
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string | string[] }>;
-}) {
-  const { id } = await params;
+async function BookDetail({ bookId }: { bookId: string }) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`
   );
   if (!response.ok) {
     if (response.status === 404) {
@@ -35,7 +21,7 @@ export default async function Page({
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
   return (
-    <div className={style.container}>
+    <section>
       <div
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
@@ -48,6 +34,32 @@ export default async function Page({
         {author} | {publisher}
       </div>
       <div className={style.description}>{description}</div>
+    </section>
+  );
+}
+
+function ReviewEditor() {
+  async function createReviewAction() {
+    "use server"; // 자동으로 서버 액션으로 설정
+    console.log("server action CALLED!");
+  }
+
+  return (
+    <section>
+      <form action={createReviewAction}>
+        <input name="content" placeholder="리뷰 내용" />
+        <input name="author" placeholder="작성자" />
+        <button type="submit">작성하기</button>
+      </form>
+    </section>
+  );
+}
+
+export default function Page({ params }: { params: { id: string } }) {
+  return (
+    <div className={style.container}>
+      <BookDetail bookId={params.id} />
+      <ReviewEditor />
     </div>
   );
 }
